7ff2439e8e83ec5dad55e83c172809f287408f26
M G <marcela.guerra@gmail.com>
2018-07-13 18:15:06 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person
+
+  def name
+    @name
+  end
+
+  def name= (name)
+    @name = name
+  end
+
+
+  def breed
+    @breed
+  end
+
+  def name= (breed)
+    @breed = breed
+  end
+
+end
